d2b12eaa6b795045c988541e3ed2260938d101e3
LeoDog896 <LeoDog896@hotmail.com>
2020-03-24 20:11:53 -0400

    Following formatting guides by zune

--- TW-NPC/src/main/java/com/steffbeard/totalwar/npc/cargo/CargoTrait.java
+++ TW-NPC/src/main/java/com/steffbeard/totalwar/npc/cargo/CargoTrait.java
@@ -6,18 +6,16 @@ import net.citizensnpcs.api.trait.TraitName;
 * CargoTrait is a marker trait that needs to be added to all dtl traders that can interact with Movecraft crafts
 */
 @TraitName("cargo")
-public class CargoTrait extends Trait
-{
+public class CargoTrait extends Trait {
 
-    public CargoTrait()
-    {
+    public CargoTrait() {
         super("cargo");
     }
 
     @Override
     public void onAttach(){
-        if(!this.npc.hasTrait(nl.thewgbbroz.dtltraders.citizens.TraderTrait.class))
-            this.npc.removeTrait(CargoTrait.class);
+        if (!npc.hasTrait(nl.thewgbbroz.dtltraders.citizens.TraderTrait.class))
+            npc.removeTrait(CargoTrait.class);
     }
 
 }
